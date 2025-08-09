4b76da23afceeff52a0f3e0373057e2814f7aaab
MHK <MHK>
2025-08-10 01:47:26 +0300

    Installing tailwind ready-components libraries packages

--- Front-End/src/app/layout.jsx
+++ Front-End/src/app/layout.jsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Provider } from "react-redux";
 import Navbar from "@/Components/navbar/page.jsx";
 import store from "@/Redux/store.js";
+import { FBFooter } from "@/Components/footer/page.jsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
         >
         <Navbar/>
         {children}
+        <FBFooter/>
 
       </body>
         </Provider>
--- Front-End/src/app/page.jsx
+++ Front-End/src/app/page.jsx
@@ -1,12 +1,16 @@
 'use client'
+import { Button } from "@/Components/chadcn-ui/button.jsx"
 import { useSelector } from "react-redux"
 
 
 
 export default function Home() {
-  let {counter} = useSelector((state) => state.counter)
+  let { counter } = useSelector((state) => state.counter)
   return <>
     <h1>Hello From Home !</h1>
     <h2>Count : {counter} </h2>
+    <div className="flex justify-center">
+      <Button className='rounded-md'>Welcome</Button>
+    </div>
   </>
 }
